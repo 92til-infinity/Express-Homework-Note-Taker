0c77e71d658ff92a01efd5a46936390c373db5b0
Scott Glover <glover.scott92@gmail.com>
2020-06-18 21:54:52 -0700

    more and more Heroku

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -1,24 +1,31 @@
 var path = require("path");
 
 
-  // ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // dont touch above
+// ---------------------------------------------------------------------------
+
+module.exports = function (app) {
+  // ---------------------------------------------------------------------------
   // ---------------------------------------------------------------------------
 
-module.exports = function(app) {
 
+  app.get("/", function (req, res) {
+    res.json(path.join(__dirname, "../public/index.html"));
+  });
+  // ---------------------------------------------------------------------------
   // ---------------------------------------------------------------------------
 
-  app.get("/notes", function(req, res) {
+  app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-    // ---------------------------------------------------------------------------
-// notes above
+  // ---------------------------------------------------------------------------
+  // notes above
 
-// homepage slash/no URL resulting page
+  // homepage slash/no URL resulting page
 
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
