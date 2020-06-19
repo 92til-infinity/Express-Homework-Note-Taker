cf459117ad6d4ddfd14650c718b880848a9ba8fc
Scott Glover <glover.scott92@gmail.com>
2020-06-18 21:32:46 -0700

    pushing before heroku

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ var app = express();
 // Set port.
 var PORT = process.env.PORT || 3000;
 
-// Set up the Express app to handle data parsing
+//EXPRESS
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTES
-// Point the server to the route files.
+
 require("./routes/APIroutes")(app);
 require("./routes/HTMLroutes")(app);
 
 // LISTENER
-// Listen on port.
+
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
